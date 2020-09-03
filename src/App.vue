f8be2ad94b584b8da022df6e6aa4e5f4cf0c88c9
Alex <alexxe33@gmail.com>
2020-09-03 16:50:48 +0200

    Test pictures

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  <div id="app">
-    <h1>Yukine Gallery</h1>
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-  </div>
+    <div id="app">
+        <h1>Yukine Gallery</h1>
+        <h3>Welcome to the Chris Zone</h3>
+        <div id="gallery">
+            <img v-for="i in img_paths" class="pic" img alt="Beautiful Chris" :src="i.pathlong">
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+      return {
+          img_paths: []
+      }
+  },
+  methods: {
+      importAll(r) {
+          r.keys().forEach(key => (this.img_paths.push({pathlong: r(key)})));
+      }
+  },
+  mounted() {
+    {
+        this.importAll(require.context('./assets/'));
+    }
+  }
 }
 </script>
 
@@ -19,5 +37,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#gallery {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    grid-auto-rows: minmax(300px, auto);
+    padding: 0 1em;
+}
+
+.pic {
+    width: 100%;
+    height: 100%;
+    object-fit: cover
 }
 </style>
